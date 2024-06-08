ae69d22e7b0d3e6e651fe9d096257037ab7f64de
shekhargrace14 <shekhargrace14@gmail.com>
2024-06-08 16:39:37 +0530

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import { FaClock, FaEnvelope, FaLocationArrow, FaPhone } from "react-icons/fa"
+import { FaLocationPin } from "react-icons/fa6"
+
+const Footer=()=>{
+    return(
+        <div>
+            <section>
+                <div className="row">
+
+                    <div className="column w-2/6">
+                        <img src="assets/logo.svg"/>
+                        <p>Pellentesque posuere orci lobortis</p>
+                        <div className="" >
+                            <FaLocationPin className="inline text-primary-color"/>
+                            <p className="inline"> <span className="font-semibold"> Address: </span> 5171 W Campbell Ave undefined Kent, Utah 53127 United States</p>
+                        </div>
+                        <div className="" >
+                            <FaPhone className="inline text-primary-color"/>
+                            <p className="inline"> <span className="font-semibold"> Call Us:  </span> (+1) - 540-025-124553</p>
+                        </div>
+                        <div className="" >
+                            <FaEnvelope className="inline text-primary-color"/>
+                            <p className="inline"> <span className="font-semibold">  Email:  </span> sale@Nest.com</p>
+                        </div>
+                        <div className="" >
+                            <FaClock className="inline text-primary-color"/>
+                            <p className="inline"> <span className="font-semibold">  Hours: </span> 10:00 - 18:00, Mon - Sat</p>
+                        </div>
+                    </div>
+                    <div className="column w-2/6"></div>
+                    <div className="column w-2/6"></div>
+                    <div className="column w-2/6"></div>
+                    <div className="column w-2/6"></div>
+                </div>
+            </section>
+        </div>
+    )
+}
+export default Footer
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,9 @@ import { FaArrowRight, FaRegPaperPlane } from "react-icons/fa";
 import FeaturedCategory from '../components/FeaturedCategory';
 import PopularProduct from '../components/PopularProducts';
 import Deal from '../components/Deal'
-import CTA from '../components/CTA';
+import Cta from '../components/Cta';
+import Usps from '../components/Usps';
+import Footer from '../components/Footer';
 // import image1 from "../assets/slider-1-1.webp";
 
 
@@ -12,16 +14,22 @@ const Home = () => {
     <div>
       <Navbar />
       
-      <section className="bg-[url('../public/assets/slider-1-1.webp')] bg-no-repeat bg-center md:py-16 py-8  md:px-24 px-4 rounded-2xl mx-4" >
-        <div className="row top xl:w-[50%] h-fit ">
-          <h1 className='lg:text-7xl sm:text-6xl text-5xl   font-primary-font font-bold text-[#253d4e]'>Welcome to Nest, Your Natural Store</h1>
-          <p className='lg:text-4xl text-1xl text-gray-600 py-8 '>Sign Up to get best deals in town</p>
-          <form className='flex justify-between items-center pl-6 bg-white rounded-full sm:w-[70%] md:mt-16'>
-            <FaRegPaperPlane className='text-gray-400 w-[10%]' />
-            <input className='md:py-5  pl-4 py-4 w-[80%] rounded-full' type='text' placeholder='Email address ' />
-            <button className='md:py-5 md:px-12 px-4 py-4 rounded-full bg-primary-color text-white hover:bg-primary-color-dark transition linear delay-150'>Subscribe</button>
-          </form>
+      <section className="bg-[url('../public/assets/bg/slider-1-1.webp')]  bg-cover bg-no-repeat bg-center md:py-16 py-8  md:px-24 px-4 rounded-2xl mx-4" >
+        <div className="container mx-auto top h-fit ">
+          <div className='row flex'>
+            <div className='column w-5/12'>
+              <h1 className='lg:text-7xl sm:text-6xl text-5xl   font-primary-font font-bold text-[#253d4e]'>Welcome to Nest, Your Natural Store</h1>
+              <p className='lg:text-4xl text-1xl text-gray-600 py-8 '>Sign Up to get best deals in town</p>
+              <form className='flex justify-between items-center pl-6 bg-white rounded-full sm:w-[70%] md:mt-16'>
+                <FaRegPaperPlane className='text-gray-400 w-[10%]' />
+                <input className='md:py-5  pl-4 py-4 w-[80%] rounded-full' type='text' placeholder='Email address ' />
+                <button className='md:py-5 md:px-12 px-4 py-4 rounded-full bg-primary-color text-white hover:bg-primary-color-dark transition linear delay-150'>Subscribe</button>
+              </form>
+            </div>
+            <div className='column'>
+            </div>
 
+          </div>
         </div>
         <div className='row bottom '></div>
       </section>
@@ -30,7 +38,7 @@ const Home = () => {
 
       <section className='lg:container mx-auto px-4'>
         <div className='row grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4'>
-          <div className="column bg-[url('../assets/bg-img-3.png')] bg-no-repeat bg-cover bg-center lg:px-12 px-8 py-16 rounded-2xl">
+          <div className="column bg-[url('../assets/bg/bg-img-3.png')] bg-no-repeat bg-cover bg-center lg:px-12 px-8 py-16 rounded-2xl">
             <div className=' w-3/4'>
               <h3 className='text-2xl font-semibold mb-8'>Everyday Fresh & Clean with Our Products</h3>
               <button className="flex flex-cols items-center gap-2 bg-primary-color text-white py-1 px-2 rounded ">Show Now <FaArrowRight/> </button>
@@ -38,7 +46,7 @@ const Home = () => {
             <div></div>
           </div>
 
-          <div className="column bg-[url('../assets/bg-img-2.png')] bg-no-repeat bg-cover bg-center lg:px-12 px-8 py-16 rounded-2xl">
+          <div className="column bg-[url('../assets/bg/bg-img-2.png')] bg-no-repeat bg-cover bg-center lg:px-12 px-8 py-16 rounded-2xl">
             <div className='w-3/4'>
               <h3 className='text-2xl font-semibold mb-8'>Make your Breakfast Healthy and Easy</h3>
               <button className="flex flex-cols items-center gap-2 bg-primary-color text-white py-1 px-2 rounded ">Show Now <FaArrowRight/> </button>
@@ -46,7 +54,7 @@ const Home = () => {
             <div></div>
           </div>
 
-          <div className="column bg-[url('../assets/bg-img-1.png')] bg-no-repeat bg-cover bg-center lg:px-12 px-8 py-16 rounded-2xl">
+          <div className="column bg-[url('../assets/bg/bg-img-1.png')] bg-no-repeat bg-cover bg-center lg:px-12 px-8 py-16 rounded-2xl">
             <div className='w-3/4'>
               <h3 className='text-2xl font-semibold mb-8'>The best Organic Products Online</h3>
               <button className="flex flex-cols items-center gap-2 bg-primary-color text-white py-1 px-2 rounded ">Show Now <FaArrowRight/> </button>
@@ -58,7 +66,10 @@ const Home = () => {
 
       <PopularProduct/>
       <Deal/>
-      <CTA/>
+      <Cta/>
+      <Usps/>
+      <Footer/>
+      
     </div>
   )
 }

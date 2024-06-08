7986a31e715ef76953be9b614aa080fdb35e1f91
shekhargrace14 <shekhargrace14@gmail.com>
2024-06-08 16:48:03 +0530

    cta adjusted

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -7,7 +7,7 @@ const Cta = () =>{
           <div className='row flex'>
             <div className='column sm:w-8/12 w-12/12'>
               <h3 className='lg:text-4xl sm:text-3xl text-2xl   font-primary-font font-black text-[#253d4e]'>Stay home & get your daily needs from our shop</h3>
-              <p className='lg:text-2xl text-1xl text-gray-600 py-8'> Start You'r Daily Shopping with <span className="text-primary-color"> Nest Mart</span></p>
+              <p className='lg:text-2xl text-1xl text-gray-600 py-8'> Start Your Daily Shopping with <span className="text-primary-color"> Nest Mart</span></p>
               <form className='flex justify-between items-center pl-6 bg-white rounded-full sm:w-[70%] md:mt-8'>
                 <FaRegPaperPlane className='text-gray-400 w-[10%]' />
                 <input className='md:py-5  pl-4 py-4 w-[80%] rounded-full' type='text' placeholder='Email address ' />
